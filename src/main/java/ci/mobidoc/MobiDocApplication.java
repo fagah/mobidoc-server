dc17b7936582261e9cff7609d33e4fff3b2337ee
Franck Janel AGAH <fagah@users.noreply.github.com>
2025-01-27 22:30:47 -0500

    Add main application class

---
+++ src/main/java/ci/mobidoc/MobiDocApplication.java
@@ -0,0 +1,11 @@
+package ci.mobidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobiDocApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MobiDocApplication.class, args);
+    }
+}
